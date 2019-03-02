b7cc00b0ec4db28dc0bd1ce6fe60ec123e00b7d8
TedLee <tedlee@Ted-Lee.local>
2019-03-02 17:23:35 +0800

    123

--- Demo_8_2/ViewController.swift
+++ Demo_8_2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     var demoView: UIView!
     var demoView2: UIView!
+    var pushFirst: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
